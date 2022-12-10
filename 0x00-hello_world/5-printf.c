58ad78a6708907daf5df9136dcae2ae6aadd533b
Wavebee <wave.bee123@gmail.com>
2022-12-10 11:44:13 -0800

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-
+#include <stdio.h> 
 /**
  * main - Entry point
  *
@@ -9,6 +8,6 @@
 int main(void)
 
 {
-	printf("with proper grammar, but the outcome is a piece of art\n");
+	printf ("with proper grammar, but the outcome is a piece of art\n,,");
 	return (0);
 }
