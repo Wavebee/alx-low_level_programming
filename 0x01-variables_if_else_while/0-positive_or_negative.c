4b9da33df11c69a0780815c40ef9845f63d10991
Wavebee <wave.bee123@gmail.com>
2022-12-11 15:57:08 -0800

    positive_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 /* more headers goes there */
 /* betty style doc for function goes there */
-/** main - Entry point
+/* main - Entry point 
  * Description: get a random number and print number
  * Return: Always 0 (Success)
  */
