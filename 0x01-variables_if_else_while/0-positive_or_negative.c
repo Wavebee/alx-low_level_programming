95930be8d0a9de287b106e07fb2af880a2a6ca18
Wavebee <wave.bee123@gmail.com>
2022-12-12 05:02:46 -0800

    positive_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,17 +4,20 @@
 
 /* more headers goes there */
 /* betty style doc for function goes there */
-/*
- *  main - Entry point
+/**
+ * main - Entry point
+ *
  * Return: Always 0 (Success)
  */
-int(char *)
+
+int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
+
 	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n == 0)
