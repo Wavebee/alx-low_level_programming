15650ea30df102f8182e4dace2be31ce3637592e
Wavebee <wave.bee123@gmail.com>
2022-12-12 20:03:44 -0800

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,29 +1,20 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
 /**
- * main - Entr point
+ * main Entry point
  *
- * Return: Always 0(Success)
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	int n;
+	char C;
 
-	int m;
-
-	srand(time(0));
-	n = rand() - RAND_MAX /2;
-	/* your code goes there */
-	m = n % 10;
-	if (m > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n, m);
-	if (m == 0)
-		printf("Last digit of %d is %d and is 0\n", n, m);
-	if (m < 6 && n != 0)
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
+	C = 'a';
+	while
+		(C <= 'z') {
+			putchar(C);
+		C++;
+		}
+	putchar('\n');
 	return (0);
 }
