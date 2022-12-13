f6ffe479cfd1d28523e0ce6b192ef496520d7426
Wavebee <wave.bee123@gmail.com>
2022-12-12 21:26:23 -0800

    3 aplhabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,7 +4,6 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	char c;
@@ -20,7 +19,7 @@ int main(void)
 		}
 	while
 		(d <= 'z') {
-			putchar(d);
+			putchar(c);
 			d++;
 		}
 	putchar('\n');
