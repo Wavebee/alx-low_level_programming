10114af6f3cb0adb7bd770e149557c2eb83ceb3b
Wavebee <wave.bee123@gmail.com>
2022-12-20 13:44:24 -0800

    100-times.c

--- 0x02-functions_nested_loops/100-main.c
+++ 0x02-functions_nested_loops/100-main.c
@@ -1,36 +1,20 @@
 #include "main.h"
 
-
-
 /**
+ * main - check the code.
  *
- *  * main - check the code.
- *
- *   *
- *
- *    * Return: Always 0.
- *
- *     */
-
+ * Return: Always 0.
+ */
 int main(void)
-
 {
 	void print_times_table(int n);
 
-	    print_times_table(3);
-
-	        _putchar('\n');
-
-		    print_times_table(5);
-
-		        _putchar('\n');
-
-			    print_times_table(98);
-
-			        _putchar('\n');
-
-				    print_times_table(12);  
-
-				        return (0);
-
+	print_times_table(3);
+	_putchar('\n');
+	print_times_table(5);
+	_putchar('\n');
+	print_times_table(98);
+	_putchar('\n');
+	print_times_table(12);
+	return (0);
 }
