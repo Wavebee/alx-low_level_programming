07157827981fa56ac36930a55f877dcfdb7aa5f0
Wavebee <wave.bee123@gmail.com>
2023-01-05 05:15:18 -0800

    0-memset

--- 0x07-pointers_arrays_strings/0-main.c
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -10,8 +10,7 @@
  */
 void simple_print_buffer(char *buffer, unsigned int size)
 {
-	char *_memset(char *s, char b, unsigned int n);
-        unsigned int i;
+       	unsigned int i;
 
         i = 0;
         while (i < size)
