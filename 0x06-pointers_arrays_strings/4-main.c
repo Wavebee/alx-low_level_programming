a50cd4d7af4ca9b1026ffc61d58e51a2ba7f77fc
Wavebee <wave.bee123@gmail.com>
2023-01-05 19:03:49 -0800

    learning to code

--- 0x06-pointers_arrays_strings/4-main.c
+++ 0x06-pointers_arrays_strings/4-main.c
@@ -1,29 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-
-/**
- * main - check the code
- * @a: an array of integers
- * @n: the number of elements to swap
- *
- * Return: nothing.
- */
-void print_array(int *a, int n)
-{
-    int i;
-
-    i = 0;
-    while (i < n)
-    {
-        if (i != 0)
-        {
-            printf(", ");
-        }
-        printf("%d", a[i]);
-        i++;
-    }
-    printf("\n");
-}
 
 /**
  * main - check the code
@@ -32,10 +7,10 @@ void print_array(int *a, int n)
  */
 int main(void)
 {
-    int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
+	void print_rev(char *s);
+	char *str;
 
-    print_array(a, sizeof(a) / sizeof(int));
-    reverse_array(a, sizeof(a) / sizeof(int));
-    print_array(a, sizeof(a) / sizeof(int));
+    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+    print_rev(str);
     return (0);
 }
---
+++ README.md
@@ -0,0 +1,1 @@
+0-puts_recursion.c
