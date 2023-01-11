0bdcb05ff8265af9379d076cf32b1a02e99c131e
Wavebee <wave.bee123@gmail.com>
2023-01-11 00:21:16 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,43 +1,50 @@
 #include <stdio.h>
-
 /**
- * main - a simple program that outputs unordered
- * combinations of two digit integers without duplicates
- *
- * Return: 0 on success
+ * main - main block
+ * Description: Print all possible combinations of two two-digit numbers.
+ * Numbers should range from 0 to 99.
+ * The two numbers should be separated by a space.
+ * All numbers should be printed with two digits. 1 should be printed as 01.
+ * Combination of numbers must be separated by a comma followed by a space.
+ * Combinations of numbers should be printed in ascending order.
+ * `00 01` and `01 00` are considered as the same combination.
+ * You can only use `putchar` to print to console.
+ * You can only use `putchar` up to 8 times.
+ * You are not allowed to use any variable of type `char`.
+ * Return: 0
  */
 int main(void)
 {
-	int ifirst;
-	int i;
-	int jfirst;
-	int j;
+	int i, j;
+	int a, b, c, d;
 
-	for (ifirst = 48; ifirst < 58; ifirst++)
+	for (i = 0; i < 100; i++)
 	{
-		for (i = 48; i < 58; i++)
+		a = i / 10; /* doubles fnum */
+		b = i % 10; /* singles fnum */
+
+		for (j = 0; j < 100; j++)
 		{
-			j = i + 1;
-			jfirst = ifirst;
-			for (; jfirst < 58; jfirst++)
+			c = j / 10; /* doubles snum */
+			d = j % 10; /* singles snum */
+
+			if (a < c || (a == c && b < d))
 			{
-				for (; j < 58; j++)
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(32);
+				putchar(c + '0');
+				putchar(d + '0');
+
+				if (!(a == 9 && b == 8))
 				{
-					putchar(ifirst);
-					putchar(i);
-					putchar(' ');
-					putchar(jfirst);
-					putchar(j);
-					if (ifirst != 57 || jfirst != 57 || i != 56 || j != 57)
-					{
-						putchar(',');
-						putchar(' ');
-					}
+					putchar(44);
+					putchar(32);
 				}
-				j = 48;
 			}
 		}
 	}
-	putchar('\n');
+	putchar(10);
+
 	return (0);
 }
