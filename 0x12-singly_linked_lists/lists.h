d579565f67ed9efe6f574fbd408e9d2c6564be78
Wavebee <wave.bee123@gmail.com>
2023-01-27 00:45:05 -0800

    0-print_list.c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,17 +1,18 @@
-#ifndef LISTS_H
-#define LISTS_H
-
+#ifndef LINKED_LIST
+#define LINKED_LIST
+#include <string.h>
 #include <stdio.h>
+#include <stddef.h>
 #include <stdlib.h>
-
 /**
-* struct list_s - singly linked list
-* @str: string - (malloc'ed string)
-* @len: length of the string
-* @next: points to the next node
-*
-* Description: singly linked list node structure
-*/
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
 typedef struct list_s
 {
 	char *str;
@@ -24,5 +25,4 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-
-#endif /* LISTS_H */
+#endif
