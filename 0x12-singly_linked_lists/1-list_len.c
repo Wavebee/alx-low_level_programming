3e82c8c53d1ec5f0e84313aa4201acd5988c00b0
Wavebee <wave.bee123@gmail.com>
2023-01-26 21:15:06 -0800

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,21 +1,23 @@
-#include <stdio.h> 
- #include "lists.h" 
-  
- /** 
-  * list_len - function with one argument 
-  * @h: const list_t pointer argument to struct 
-  * 
-  * Description: returns the number of elements in a linked list 
-  * Return: number of elements 
-  */ 
- size_t list_len(const list_t *h) 
- { 
-         unsigned int count = 0; 
-  
-         while (h != NULL) 
-         { 
-                 h = h->next; 
-                 count++; 
-         } 
-         return (count); 
- }
+#include "lists.h"
+
+/**
+* list_len - a function that returns the number of elements in a
+* linked list_t list
+* @h: a linked list
+* Return: returns the number of elements in h
+*/
+size_t list_len(const list_t *h)
+{
+	int i = 0;
+
+	if (h == NULL)
+		return (0);
+
+	while (h != NULL)
+	{
+		if (h->str != NULL)
+			i++;
+		h = h->next;
+	}
+	return (i);
+}
